050ec587a78ed3392319d3b1787c404ca69ad10f
asnegur <bbs.md@mail.ru>
2024-03-13 23:22:01 +0200

    first commit

---
+++ next-amp-graphql-tol/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextgrapghqltol1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
